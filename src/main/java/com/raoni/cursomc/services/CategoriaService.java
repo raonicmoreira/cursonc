32f9f54359216c4dc3f49be4b0f36c552427833d
Raoni Moreira <raonicmoreira@gmail.com>
2018-03-10 18:53:11 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/raoni/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.raoni.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.raoni.cursomc.domain.Categoria;
+import com.raoni.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		
+		Categoria obj = repo.findOne(id);
+		return obj;
+		
+		
+	}
+
+}
