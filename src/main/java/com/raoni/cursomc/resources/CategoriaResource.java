1dfc9e60b6de4a4ff0a1d0094a3fa5fa4a95e7e5
Raoni Moreira <raonicmoreira@gmail.com>
2018-03-12 22:08:31 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/raoni/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/raoni/cursomc/resources/CategoriaResource.java
@@ -18,7 +18,7 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	
-	@RequestMapping(value="/(id)", method=RequestMethod.GET)
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		
