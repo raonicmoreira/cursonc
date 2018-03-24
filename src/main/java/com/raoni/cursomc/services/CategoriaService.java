03f8256cc09cf17946e4ad11e85cb1c4bc116852
Raoni Moreira <raonicmoreira@gmail.com>
2018-03-24 11:08:49 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/raoni/cursomc/services/CategoriaService.java
+++ src/main/java/com/raoni/cursomc/services/CategoriaService.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.raoni.cursomc.domain.Categoria;
@@ -51,5 +54,10 @@ public class CategoriaService {
 
 	public List<Categoria> findAll(){
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderby, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderby);
+		return repo.findAll(pageRequest);
 	}
 }
