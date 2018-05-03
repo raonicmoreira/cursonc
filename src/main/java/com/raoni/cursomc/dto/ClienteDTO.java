30c778c7f132182918abef9f05bfa869781741df
Raoni Moreira <raonicmoreira@gmail.com>
2018-05-02 21:18:58 -0300

    Validacao Customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/com/raoni/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/raoni/cursomc/dto/ClienteDTO.java
@@ -7,9 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import com.raoni.cursomc.domain.Cliente;
-import com.raoni.cursomc.services.validation.ClienteInsert;
+import com.raoni.cursomc.services.validation.ClienteUpdate;
 
-@ClienteInsert
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
